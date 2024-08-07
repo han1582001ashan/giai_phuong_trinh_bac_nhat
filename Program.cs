ff5471d461c100afe700e1128bc984c9546d6ab8
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-07 20:33:03 +0700

    nop bai

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LinearEquationResolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinearEquation Resolver");
+            Console.WriteLine("Given a equation as 'a * x +b = 0', please enter constants");
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("The solution is all x");
+                }
+                else
+                {
+                    Console.WriteLine("Not solution");
+                }
+            }
+            else
+            {
+                double sol = -b / a;
+                Console.WriteLine("The solution is :" + sol);
+            }
+        }
+    }
+}
